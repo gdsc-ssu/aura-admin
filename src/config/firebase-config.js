3fdda17f2145de0d86bca795dfc550775857bada
gomjellie <gomjellie@gmail.com>
2021-09-14 17:02:32 +0900

    update firebase configs

--- src/config/firebase-config.js
+++ src/config/firebase-config.js
@@ -1,9 +1,9 @@
 export default {
-    apiKey: "AIzaSyCOl0qzkhCDmyWeeIT8AZe4xyJJlIfU5gE",
-    authDomain: "myaurapp.firebaseapp.com",
-    databaseURL: "https://myaurapp.firebaseio.com",
-    projectId: "myaurapp",
-    storageBucket: "myaurapp.appspot.com",
-    messagingSenderId: "419511997060",
-    appId: "1:419511997060:web:ce7aa0d308f6b484f868d1"
-}
+  apiKey: 'AIzaSyBMwzqRRI8JV40M5U0XVj9WL3B2f47Zz0g',
+  authDomain: 'gdsc-ssu.firebaseapp.com',
+  projectId: 'gdsc-ssu',
+  storageBucket: 'gdsc-ssu.appspot.com',
+  messagingSenderId: '1025647897879',
+  appId: '1:1025647897879:web:1480039c4eb7d9aeca5b52',
+  measurementId: 'G-WB9SKLWV4G',
+};
